ef7ebe894cd4c04c4e997bd394d0c312cf50ed18
Ruari Day <ruariday@gmail.com>
2019-12-16 11:39:07 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,10 @@ end
 
 def print_first_directors_movie_titles(directors_database)
   row_index = 0
-    while row_index < directors_database.length do
-      element_index = 0
-    end
+  while row_index < directors_database.length do
+    element_index = 0
+    while 
+  end
+  end
+  row_index += 1
 end
